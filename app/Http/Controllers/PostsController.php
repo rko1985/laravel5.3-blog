673d0e8d28cba1e8b9d02c8bb97622ce30d2bf64
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-09-10 12:25:05 -1000

    uploading posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Post;
+use Session;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -23,7 +26,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        return view('admin.posts.create')->with('categories', Category::all());
     }
 
     /**
@@ -37,9 +40,26 @@ class PostsController extends Controller
         $this->validate($request, [
             'title' => 'required',
             'featured' => 'required|image',
-            'content' => 'required'
+            'content' => 'required',
+            'category_id' => 'required'
+        ]);
+        
+        //getting the image
+        $featured = $request->featured;
+        //creating a unique name for image
+        $featured_new_name = time().$featured->getClientOriginalName();
+        //move file to uploads folder
+        $featured->move('uploads/posts', $featured_new_name);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'featured' => 'uploads/posts/' . $featured_new_name,
+            'category_id' => $request->category_id
         ]);
 
+        Session::flash('success', 'Post created successfully');
+        
         dd($request->all());
     }
 
--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'content',
+        'category_id',
+        'featured'
+    ];
+
     public function category(){
         return $this->belongsTo('App\Category');
     }
