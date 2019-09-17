3b20c2730d10158066b8b64f80cac47703b04ab2
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-09-17 08:49:21 -1000

    made views look better

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 
     <div class="panel panel-default">
+        <div class="panel-heading">
+            Categories
+        </div>
         <div class="panel-body">
             <table class="table table-hover">
                 <thead>
@@ -17,21 +20,27 @@
                     </th>
                 </thead>
                 <tbody>
-                    @foreach ($categories as $category)
+                    @if ($categories->count() > 0)
+                        @foreach ($categories as $category)
+                            <tr>
+                                <td>{{$category->name}}</td>
+                                <td>
+                                    <a href="{{route('category.edit', ['id' => $category->id])}}" class="btn btn-info btn-xs">
+                                        Edit
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{route('category.delete', ['id' => $category->id])}}" class="btn btn-danger btn-xs">
+                                        Delete
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach                       
+                    @else
                         <tr>
-                            <td>{{$category->name}}</td>
-                            <td>
-                                <a href="{{route('category.edit', ['id' => $category->id])}}" class="btn btn-info btn-xs">
-                                    Edit
-                                </a>
-                            </td>
-                            <td>
-                                <a href="{{route('category.delete', ['id' => $category->id])}}" class="btn btn-danger btn-xs">
-                                    Delete
-                                </a>
-                            </td>
+                            <th colspan="5" class="text-center">No Published Posts</th>
                         </tr>
-                    @endforeach
+                    @endif
                 </tbody>
             </table>
         </div>
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 
     <div class="panel panel-default">
+        <div class="panel-heading">
+                Published Posts
+        </div>
         <div class="panel-body">
             <table class="table table-hover">
                 <thead>
@@ -20,14 +23,21 @@
                     </th>
                 </thead>
                 <tbody>
-                    @foreach ($posts as $post)
+                    @if ($posts->count() > 0)
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td><img src="{{$post->featured}}" width="90px" alt="{{$post->title}}"></td>
+                                <td>{{$post->title}}</td>
+                                <td><a href="{{route('post.edit', ['id' => $post->id])}}" class="btn btn-info">Edit</a></td>
+                                <td><a href="{{route('post.delete', ['id' => $post->id])}}" class="btn btn-danger">Trash</a></td>
+                            </tr>
+                        @endforeach
+                    @else
                         <tr>
-                            <td><img src="{{$post->featured}}" width="90px" alt="{{$post->title}}"></td>
-                            <td>{{$post->title}}</td>
-                            <td><a href="{{route('post.edit', ['id' => $post->id])}}" class="btn btn-info">Edit</a></td>
-                            <td><a href="{{route('post.delete', ['id' => $post->id])}}" class="btn btn-danger">Trash</a></td>
+                            <th colspan="5" class="text-center">No Published Posts</th>
                         </tr>
-                    @endforeach
+                    @endif
+                    
                 </tbody>
             </table>
         </div>
