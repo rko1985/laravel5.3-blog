41add42381ccdd42941550cf4accfa887b898a4e
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-11-01 10:51:05 -1000

    middleware to check if admin

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use Session;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
     /**
      * Display a listing of the resource.
      *
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,8 +89,10 @@
                         <li class="list-group-item"><a href="{{route('home')}}">Home</a></li>
                         <li class="list-group-item"><a href="{{route('categories')}}">Categories</a></li>
                         <li class="list-group-item"><a href="{{route('tags')}}">Tags</a></li>
-                        <li class="list-group-item"><a href="{{route('users')}}">Users</a></li>
-                        <li class="list-group-item"><a href="{{route('user.create')}}">New User</a></li>
+                        @if(Auth::user()->admin)
+                            <li class="list-group-item"><a href="{{route('users')}}">Users</a></li>
+                            <li class="list-group-item"><a href="{{route('user.create')}}">New User</a></li>
+                        @endif                        
                         <li class="list-group-item"><a href="{{route('tag.create')}}">Create Tag</a></li>
                         <li class="list-group-item"><a href="{{route('posts')}}">All posts</a></li>
                         <li class="list-group-item"><a href="{{route('posts.trashed')}}">All trashed posts</a></li>
