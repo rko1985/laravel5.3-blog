a42a6058d1c1c472bf9621ba172c54788e8e293d
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-11-08 09:32:32 -1000

    fixed wysiwyg in edit post

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -284,34 +284,7 @@
 
 <!-- Subscribe Form -->
 
-<div class="container-fluid bg-green-color">
-    <div class="row">
-        <div class="container">
-            <div class="row">
-                <div class="subscribe scrollme">
-                    <div class="col-lg-6 col-lg-offset-5 col-md-6 col-md-offset-5 col-sm-12 col-xs-12">
-                        <h4 class="subscribe-title">Email Newsletters!</h4>
-                        <form class="subscribe-form" method="post" action="/subscribe">
-                            {{ csrf_field() }}
-                            <input class="email input-standard-grey input-white" name="email" required="required" placeholder="Your Email Address" type="email">
-                            <button class="subscr-btn" method="submit">subscribe
-                                <span class="semicircle--right"></span>
-                            </button>
-                        </form>
-                        <div class="sub-title">Sign up for new Seosignt content, updates, surveys & offers.</div>
-
-                    </div>
-
-                    <div class="images-block">
-                        <img src="app/img/subscr-gear.png" alt="gear" class="gear">
-                        <img src="app/img/subscr1.png" alt="mail" class="mail">
-                        <img src="app/img/subscr-mailopen.png" alt="mail" class="mail-2">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@include('includes.form')
 
 <!-- End Subscribe Form -->
 </div>
@@ -361,21 +334,7 @@
 
 <!-- Overlay Search -->
 
-<div class="overlay_search">
-    <div class="container">
-        <div class="row">
-            <div class="form_search-wrap">
-                <form method="GET" action="/results">
-                    <input class="overlay_search-input" name="query" placeholder="Type and hit Enter..." type="text">
-                    <a href="#" class="overlay_search-close">
-                        <span></span>
-                        <span></span>
-                    </a>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+@include('includes.search')
 
 <!-- End Overlay Search -->
 
@@ -386,7 +345,6 @@
 <script src="{{ asset('app/js/swiper.jquery.min.js') }}"></script>
 <script src="{{ asset('app/js/theme-plugins.js') }}"></script>
 <script src="{{ asset('app/js/main.js') }}"></script>
-<script src="{{ asset('app/js/form-actions.js') }}"></script>
 <script src="{{asset('js/toastr.min.js')}}"></script>
 <script src="{{ asset('app/js/velocity.min.js') }}"></script>
 <script src="{{ asset('app/js/ScrollMagic.min.js') }}"></script>
