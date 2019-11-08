3b281e65b820065179ea68d4fd6a0c436211bf52
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-11-08 07:10:57 -1000

    AddThis installation into blog

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -159,5 +159,9 @@
 
 <!-- ...end JS Script -->
 
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5dc59ecf8474f812"></script>
+
+
 </body>
 </html>
